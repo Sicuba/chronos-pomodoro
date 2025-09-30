b33b8364275aa4c67fbaf531b63010fdb4609224
Josevaldo Sicuba <josevaldosicuba@gmail.com>
2025-09-30 15:44:14 +0100

    up modify main

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export function App(){
+    return(
+        <>
+        <h1>
+        Ola Mundo (APP)    
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita nisi quam similique blanditiis adipisci, </p> 
+       <p>commodi quibusdam nulla molestias cumque vero quia ratione numquam consectetur dolorum architecto optio qui. Soluta, alias.</p>
+        </>
+    )
 }
-
-export default App
