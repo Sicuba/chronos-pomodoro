630a6ca6f99f9648c628fab080b1972759d49496
Josevaldo Sicuba <josevaldosicuba@gmail.com>
2025-10-02 15:03:19 +0100

    add css and css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { Heading } from "./components/Heading"
+
 export function App(){
     return(
         <>
-        <h1>
-        Ola Mundo (APP)    
-        </h1>
+        <Heading/>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita nisi quam similique blanditiis adipisci, </p> 
        <p>commodi quibusdam nulla molestias cumque vero quia ratione numquam consectetur dolorum architecto optio qui. Soluta, alias.</p>
         </>
