9abdf60866084ab60d4d7ad44a0b0aa2a47e5e98
Josevaldo Sicuba <josevaldosicuba@gmail.com>
2025-10-02 15:33:22 +0100

    add props and childrens

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Heading } from "./components/Heading"
 
+import './styles/theme.css';
+import './styles/global.css';
+
 export function App(){
     return(
         <>
-        <Heading/>
+        <Heading color="white" vars={2}>Ola com children</Heading>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita nisi quam similique blanditiis adipisci, </p> 
        <p>commodi quibusdam nulla molestias cumque vero quia ratione numquam consectetur dolorum architecto optio qui. Soluta, alias.</p>
         </>
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,11 @@
 import styles from './Heading.module.css';
 
-export function Heading(){
+export function Heading({children, vars, color} : {children:React.ReactNode, vars:number, color:string}){
     return(
-        <h1 className={`${styles.cyan}`}>Heading MAior!!</h1>
+        <>
+        <h1 className={`${styles.cyan}`}>{children}</h1>
+        <h1>Cor: {color}</h1>
+        <h1>Var: {vars}</h1>
+        </>
     )
 }
